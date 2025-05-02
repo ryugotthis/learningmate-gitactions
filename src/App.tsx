0efec701300e81beec4d9a5de6d88421b2da19c4
Subin <ryucandoit6@gmail.com>
2025-05-02 10:09:28 +0900

    docs: useEffect 주석 추가

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,14 @@ const App = ({ children }: { children?: React.ReactNode }) => {
   useEffect(() => {
     const refreshTokenOnLoad = async () => {
       if (!isLoggedIn) {
-        setAuthReady();
-        return;
-      } // 로그인 상태가 아닐 경우 실행 안 함
+        // 로그인이 안되어 있는 경우
+        setAuthReady(); // 토큰 재발급 필요없으니 허용 상태로
+        return; // 토큰 발급 없이 반환
+      }
       if (!accessToken) {
-        //  새로고침 감지: accessToken 없음 → 재발급 시도;
+        //  로그인 후 새로고침 감지: accessToken 없음 → 재발급 시도;
         try {
-          await reissueToken(); // ✅ mutateAsync() 사용하여 실행
+          await reissueToken(); // 토큰 재발급
         } catch (error) {
           console.error('🚨 새로고침 시 토큰 갱신 실패, 로그아웃 처리 필요');
           setIsLoggedIn(false); // 로그아웃 상태 변경
