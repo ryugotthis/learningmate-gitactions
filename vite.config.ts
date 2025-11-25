7c3211dcb72e50377a1637bb98f55054156a3759
subin <ryucandoit6@gmail.com>
2025-11-25 10:25:33 +0900

    fix: ESM 환경에서 __dirname이 동작하도록 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
-// import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-// svg파일을 ReactComponent로 처리하도록 설정
-// import svgr from 'vite-plugin-svgr';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
