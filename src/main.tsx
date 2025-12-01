07905a17583cac999bd49691b8ba3e8b6f02f561
subin <ryucandoit6@gmail.com>
2025-12-01 18:42:44 +0900

    chore: main.tsx에 유지보수 향상을 위한 주석 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@
 import ReactDOM from 'react-dom/client';
 import { TanStackQueryProvider } from './app/providers/TanstackQueryProvider';
 import { RouterProvider } from './app/providers/RouterProvider';
-import { HelmetProvider } from 'react-helmet-async'; // seo 개선 위해 사용
+import { HelmetProvider } from 'react-helmet-async'; // // 페이지별 메타태그 관리 (SPA SEO 개선)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
+
+  // HelmetProvider: 페이지별 title, description 등 Meta 관리 전역 컨텍스트
   <HelmetProvider>
+
+    {/* 전역 React Query Provider (데이터 캐싱 / 서버 상태 관리) */}
     <TanStackQueryProvider>
+
+      {/* 라우터 전역 Provider - 모든 페이지 이동 처리 */}
       <RouterProvider />
     </TanStackQueryProvider>
   </HelmetProvider>
