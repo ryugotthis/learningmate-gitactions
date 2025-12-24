eb514f857e7de0a50f4641c06fab816e5aeedc8b
subin <ryucandoit6@gmail.com>
2025-12-24 13:21:14 +0900

    chore: 디버그용 console.log 제거

--- src/entities/auth/api/createProfileImage.ts
+++ src/entities/auth/api/createProfileImage.ts
@@ -12,7 +12,6 @@ export const apiClient = axios.create({
 // ✅ 요청 인터셉터 추가 (accessToken 자동 추가)
 apiClient.interceptors.request.use((config) => {
   const { accessToken } = useAuthStore.getState(); // ✅ 상태에서 직접 가져오기
-  console.log('토큰', accessToken);
   if (accessToken) {
     config.headers.Authorization = `Bearer ${accessToken}`;
   }
