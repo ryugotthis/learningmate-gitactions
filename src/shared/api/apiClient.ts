a8f1752a0374011dfe184522a8fd9eba638cecb3
subin <ryucandoit6@gmail.com>
2026-02-13 10:09:53 +0900

    fix: increase axios timeout for lecture API request

--- src/shared/api/apiClient.ts
+++ src/shared/api/apiClient.ts
@@ -1,11 +1,11 @@
 // Axios 기본 설정
-import axios from 'axios';
+import axios from "axios";
 
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
   withCredentials: true, // 쿠키를 포함한 요청 허용
-   timeout: 3000, // 3초 타임아웃 설정
+  timeout: 6000, // 3초 타임아웃 설정
 });
--- src/shared/api/authApiClient.ts
+++ src/shared/api/authApiClient.ts
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import { useAuthStore } from '../store/authstore';
-import { reissue } from '../../entities/auth/api/reissue';
+import axios from "axios";
+import { useAuthStore } from "../store/authstore";
+import { reissue } from "../../entities/auth/api/reissue";
 
 export const authApiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL, //API 기본 URL
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
   withCredentials: true, // 쿠키를 포함한 요청 허용
-    timeout: 3000, // 3초 타임아웃 설정
+  timeout: 6000, // 3초 타임아웃 설정
 });
 
 // 요청 인터셉터 추가 (accessToken 자동 추가)
@@ -38,18 +38,18 @@ authApiClient.interceptors.response.use(
       } catch (reissueError) {
         useAuthStore.getState().clearAccessToken();
         useAuthStore.getState().setIsLoggedIn(false);
-        alert('세션이 만료되었습니다. 다시 로그인해주세요.');
-        window.location.href = '/login';
+        alert("세션이 만료되었습니다. 다시 로그인해주세요.");
+        window.location.href = "/login";
         return Promise.reject(reissueError);
       }
     }
 
     if (error.response?.status === 403) {
-      alert('접근 권한이 없습니다.');
+      alert("접근 권한이 없습니다.");
       // 필요하면 다른 페이지로 이동도 가능
       // window.location.href = '/unauthorized';
     }
 
     return Promise.reject(error);
-  }
+  },
 );
